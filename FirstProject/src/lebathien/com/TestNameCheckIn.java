4218dedb4547a2975be12315a8ba51a9f3e90764
thien.le <thien.le@ifactory>
2019-12-08 11:27:10 +0700

    Name check in

---
+++ FirstProject/src/lebathien/com/TestNameCheckIn.java
@@ -0,0 +1,15 @@
+package lebathien.com;
+
+public class TestNameCheckIn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		nameCheckIn("Thien le");
+	}
+	
+	static void nameCheckIn(String a){
+		System.out.println("Check in by " + a);
+	}
+
+}
