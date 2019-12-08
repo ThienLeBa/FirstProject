a4bdc71a4f2d13e6dcf01e0c2f0d738ce6e85289
Admin <Admin@DESKTOP-KMNS09Q>
2019-12-08 11:12:02 +0700

    Signed-off-by: Admin <Admin@DESKTOP-KMNS09Q>

---
+++ FirstProject/src/lebathien/com/TestMain.java
@@ -0,0 +1,11 @@
+package lebathien.com;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello \nThien Le \niFactory \n");
+	}
+
+}
