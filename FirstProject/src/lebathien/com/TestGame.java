22430d9e8ffd540ce8de3c219fec441ae14a5bcf
Admin <Admin@DESKTOP-KMNS09Q>
2019-12-08 11:22:39 +0700

    Check in Game Class

---
+++ FirstProject/src/lebathien/com/TestGame.java
@@ -0,0 +1,15 @@
+package lebathien.com;
+
+public class TestGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		choiGame();
+	}
+	
+	static void choiGame(){
+		System.out.println("Choi Game nhe!");
+	}
+
+}
